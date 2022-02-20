66fc6d9a8e93c48c8601627b4ba356fa39af7b91
minchan <kroud153@example.com>
2022-02-21 04:20:36 +0900

    이미지 문제

--- js/background.js
+++ js/background.js
@@ -1,10 +1,10 @@
 const images = ["0.jpg","1.jpg","2.jpg","3.jpg"];
 
-//const backgroundImage = document.createElement("img");
-const body = document.body;
+const backgroundImage = document.createElement("img");
+//const body = document.body;
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-//backgroundImage.src = `img/${chosenImage}`;
+backgroundImage.src = `img/${chosenImage}`;
 
 
 document.body.appendChild(backgroundImage);
